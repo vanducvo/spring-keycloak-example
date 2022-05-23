79944058ecdabb530a37fcb0ebf41ec8314a322e
Duc Vo <ducvo@ducvo.ducvo.dev>
2022-05-23 23:05:58 +0700

    feat: code base spring

---
+++ src/main/java/com/ducvo/springkeycloakexample/SpringKeycloakExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ducvo.springkeycloakexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKeycloakExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringKeycloakExampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ducvo/springkeycloakexample/SpringKeycloakExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ducvo.springkeycloakexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKeycloakExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
